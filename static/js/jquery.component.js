8f04b5dc48b508ae58cc13f2533ecd25bba2cf66
Rango <yuanoook@foxmail.com>
2016-06-23 17:44:07 +0800

    update cache logical

--- static/js/jquery.component.js
+++ static/js/jquery.component.js
@@ -2,37 +2,34 @@
   var components = {};
   var shares = {};
 
+  //safe_name 里面不含有 '.'，以防止 pullValue pushValue 使用冲突
   $.extend({
-    defineComponentOnce: defineComponentOnce,
     defineComponent: function(name,src,version){
-      name = encodeComponentName(name);
-      if( hasComponent(name) )
-      throw new Error('component '+name+' has exsited');
-      return defineComponent(name,src,version);
+      var safe_name = $.safeKey(name);
+      if( hasComponent(safe_name) )
+      throw new Error('component '+safe_name+' has exsited');
+      return defineComponent(safe_name, src, version);
     }
   });
 
   $.fn.extend({
     parseAsComponent: function(name){
-      var original_name = name;
-      name = encodeComponentName(name);
-      !hasComponent(name) && defineComponent(
-        name,
-        this.attr('src') || original_name,
+      safe_name = $.safeKey(name);
+      !hasComponent(safe_name) && defineComponent(
+        safe_name,
+        this.attr('src') || name,
         $(this).getVersion()
       );
-      parseComponent.call(this,name);
+      parseComponent.call(this, safe_name);
       return this;
     }
   });
 
-  function defineComponentOnce(name,src,version){
-    name = encodeComponentName(name);
-    !hasComponent(name) && defineComponent(name,src);
+  function parseComponent(name){
+    componentReady(name, produceComponent.bind(this,name));
   }
 
   function defineComponent(name,src,version){
-    name = encodeComponentName(name);
     //预编译模版
     $.pushValue(components,name,{
       ready: false,
@@ -40,7 +37,7 @@
       instances: []
     });
 
-    getComponentResource(name,src,version,function(res){
+    $.cacheSrcText(src,version).then(function(res){
       $.pushValue(components,name+'.template',res);
       $.pushValue(components,name+'.ready',true);
       var tasks = $.pullValue(components,name+'.tasks',[]);
@@ -48,48 +45,20 @@
     });
   }
 
-  function encodeComponentName(name){
-    return name.replace(/\./g,'__').replace(/\//g,'_-');
+  function componentReady(name,func){
+    $.pullValue(components,name+'.ready',false) ? func() : $.pushValue(
+      components,
+      name+'.tasks.'+$.pullValue(components,name+'.tasks.length',0),
+      func
+    );
   }
 
-  function getComponentResource(name,src,version,callback){
-    var components_cache_key = 'jquery_components';
-    var component_store_key = [name,src,version].join('::');
-
-    var cached_components = $.getLocalJsonData(components_cache_key) || {};
-
-    cached_components[component_store_key] ? (
-      callback && callback(cached_components[component_store_key])
-    ) : $.get(src,function(res){
-
-      //clear cache out of version
-      for(var key in cached_components){
-        if(cached_components.hasOwnProperty(key) && key.split('::')[0]==name){
-          // console.log(key);
-          delete cached_components[key];
-          break;
-        }
-      }
-
-      cached_components[component_store_key] = res;
-      $.saveLocalJsonData(components_cache_key,cached_components);
-
-      callback && callback(cached_components[component_store_key]);
-    });
-  }
-
-  function parseComponent(name){
-    execOnComponent(name,resolveComponent.bind(this,name));
-  }
-
-  function resolveComponent(name){
+  function  produceComponent(name){
     var cid = $.randomId(name+'_');
     var component = components[name];
     var template = component['template'];
     var tmp_dom = $('<div>').html(template);
 
-    //css
-    //all component instances share one style
     var style = tmp_dom.children('style');
     if(style.length && !component.style){
       var less_str = '[cname='+name+']'+style.html();
@@ -100,11 +69,11 @@
       component.style = style;
     }
 
-    // scope_scripts, combine in one function named main and run after every instance created.
-    var scope_scripts = tmp_dom.children('script:not([src]),script[src][scope]');
     // global_scripts, just run one time before scope_scripts;
     var global_scripts = tmp_dom.children('script[src]:not([scope])');
-    
+    // scope_scripts, combine in one function named main and run after every instance created.
+    var scope_scripts = tmp_dom.children('script:not([src]),script[src][scope]');
+
     var dom = tmp_dom.children(':not(style):not(script):first');
     dom = dom.length ? dom : this;
     dom.attr('cid',cid).attr('cname',name);
@@ -132,14 +101,6 @@
       var main = new Function('me','me=this;'+res);
       main.call(dom);
     }));
-  }
-
-  function execOnComponent(name,func){
-    $.pullValue(components,name+'.ready',false) ? func() : $.pushValue(
-      components,
-      name+'.tasks.'+$.pullValue(components,name+'.tasks.length',0),
-      func
-    );
   }
 
   function hasComponent(name){
